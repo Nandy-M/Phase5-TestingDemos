6678ad2ee8a3fb8804efd599d93038ef8189a4e3
Nandhini <nandhinimurugesan03@gmail.com>
2022-10-07 11:25:52 +0530

    changes

---
+++ P5deployproject/src/main/java/com/example/demo/P5deployprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P5deployprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P5deployprojectApplication.class, args);
+	}
+
+}
